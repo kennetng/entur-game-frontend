36b21fc1fb90e77280a5d02598a591bd0a6661a0
Kenneth <kennethgngo@gmail.com>
2024-06-05 18:00:53 +0200

    Update EnInsertTur.tsx

--- src/components/EnInsertTur.tsx
+++ src/components/EnInsertTur.tsx
@@ -19,7 +19,7 @@ export function EnInsertTur(): JSX.Element {
     )
 }
 
-const SHIFTING_HEADER_WORDS = ['Gjøvik', 'Innlands', 'NTNU']
+const SHIFTING_HEADER_WORDS = ['ODA', 'SPILL', 'KONFERANSE']
 
 function ShiftingHeader() {
     const [currentIndex, setIndex] = React.useState(0)
